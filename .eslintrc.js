11ec18b5833dbb7237f1a77a05b0764ea6dc9d93
huanghaichuan <714587274@qq.com>
2019-09-04 15:34:30 +0800

    搭建基础配置项

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "parserOptions": {
+        "ecmaVersion": 7,
+        "sourceType": "module"
+      },
+  "extends": ["standard","plugin:jest/recommended"]
+};
---
+++ test/string/index.test.js
@@ -0,0 +1,5 @@
+import reduce from '../../code/string/index'
+
+test('reduces 3-2 to equal 1', ()=>{
+    expect(reduce(3,2)).toBe(1)
+})
